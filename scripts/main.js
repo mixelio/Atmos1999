c3ce63e1773af286666f19f2b40b3b7140d1a2e0
Mykhailo Hubko <mixelios@gmail.com>
2023-07-16 09:56:42 +0200

    adaptiv

--- scripts/main.js
+++ scripts/main.js
@@ -13,4 +13,19 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+const menuOpen = document.querySelector('.menu__opener');
+const menuClose = document.querySelector('.menu__closer');
+const menuBody = document.querySelector('.header__menu');
 
+if (menuOpen) {
+  menuOpen.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    menuOpen.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+  menuClose.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    menuOpen.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
